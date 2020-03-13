7654523d3573c8b91ef75a2e1c103a0eaba5e0f7
r7598412 <r7598412@gmail.com>
2020-03-12 22:53:24 -0700

    Line cursor update

--- Team_05/src/Connection.java
+++ Team_05/src/Connection.java
@@ -24,30 +24,23 @@ public class Connection {
 	public void setSourceX(int sourceX) {
 		this.sourceX = sourceX;
 	}
-
+	
 	public int getSourceY() {
 		return sourceY;
 	}
-
 	public void setSourceY(int sourceY) {
 		this.sourceY = sourceY;
 	}
-
 	public int getDestX() {
 		return destX;
 	}
-
 	public void setDestX(int destX) {
 		this.destX = destX;
 	}
-
 	public int getDestY() {
 		return destY;
 	}
-
 	public void setDestY(int destY) {
 		this.destY = destY;
 	}
-
-	
 }
--- Team_05/src/Line.java
+++ Team_05/src/Line.java
@@ -19,6 +19,8 @@ public class Line extends JPanel{
 		/* make the line or text smooth */
 		g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
 		g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+		g2.drawLine(destX-5, destY-5, destX, destY);
 		g2.drawLine(sourceX,sourceY,destX,destY);
+		g2.drawLine(destX-5, destY+5, destX, destY);
 	}
 }
