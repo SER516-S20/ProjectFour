9ead09025f020eadc8d725e1084514075b852de9
hongqi Zhang <hzhan193@asu.edu>
2020-03-13 05:20:25 -0700

    add new function

--- Team_05/src/Box.java
+++ Team_05/src/Box.java
@@ -2,21 +2,46 @@ import java.awt.Point;
 
 public class Box {
 	private static Box instance = null;
-	private String text = null;
-	private Point startPos = null;
-	private Point endPos = null;
+	private String text;
+	private Point startPos;
+	private Point endPos;
+	private Connection conn;
+
+	private Box() {
+		text = null;
+		startPos = null;
+		endPos = null;
+		conn = null;
+	}
+	public static Box getInstance() {
+		if(instance == null) {
+			instance = new Box();
+		}
+		return instance;
+	}
+	public Point getStartPos() {
+		return startPos;
+	}
+	public void setStartPos(Point startPos) {
+		this.startPos = startPos;
+	}
+	public Point getEndPos() {
+		return endPos;
+	}
+	public void setEndPos(Point endPos) {
+		this.endPos = endPos;
+	}
 	public String getText() {
 		return text;
 	}
 	public void setText(String text) {
 		this.text = text;
 	}
-	private Box() {}
-	public static Box getInstance() {
-		if(instance == null) {
-			instance = new Box();
-		}
-		return instance;
+	public Connection getConn() {
+		return conn;
+	}
+	public void setConn(Connection conn) {
+		this.conn = conn;
 	}
 	
 }
--- Team_05/src/Model.java
+++ Team_05/src/Model.java
@@ -1,4 +1,42 @@
+import java.awt.Component;
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Model {
-
+	private static List<Connection> connections = new ArrayList<Connection>();
+	
+	public Model() {
+		
+	}
+	
+	/*
+	 * this method is used to remove the existed button by
+	 * clicking the mouse right button
+	 */
+	public Component removeComponent(Component btnBox) {
+		
+		return btnBox;
+	}
+	
+	/*
+	 * this method is used to add a new connection to the collection
+	 */
+	public void addConnection(Connection conn) {
+		
+	}
+	public boolean updateConnectionList() {
+		return true;
+	}
+	
+	/*
+	 * get all the connections 
+	 */
+	public List<Connection> getConnectionCollection(){
+		return null;
+	}
+	
+	public boolean isDotUsed(Point aDot) {
+		return true;
+	}
 }
