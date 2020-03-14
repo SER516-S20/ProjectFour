5727966b3fe21b7f4878cc0a399202b700db9386
Yijian Hu <yijianhu@asu.edu>
2020-03-14 05:00:57 -0700

    Added new right panel related
    Added new right panel related

--- Team_05/src/Model.java
+++ Team_05/src/Model.java
@@ -1,10 +1,14 @@
 import java.awt.Component;
 import java.awt.Point;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 public class Model {
 	private static List<Connection> connections = new ArrayList<Connection>();
+	private static RightTabbedPane rightTabbedPane;
+	private static Hashtable<String, WorkingAreaTab> tabs;
+	
 	
 	public Model() {
 		
@@ -38,5 +42,34 @@ public class Model {
 	
 	public boolean isDotUsed(Point aDot) {
 		return true;
+	}
+	
+	public void setRightTabbedPane(RightTabbedPane rightTabbedPane)
+	{
+		this.rightTabbedPane = rightTabbedPane;
+	}
+	
+	public RightTabbedPane getRightTabbedPane()
+	{
+		return rightTabbedPane;
+	}
+	
+	public boolean addWorkingAreaTab(WorkingAreaTab tab)
+	{
+		boolean added = false;
+		if(tabs == null)
+		{
+			tabs = new Hashtable<String, WorkingAreaTab>();
+		}
+		if(tab.getName() == null)
+		{
+			added = false;
+		}
+		else if(!tabs.containsKey(tab.getName()))
+		{
+			tabs.put(tab.getName(), tab);
+			added = true;
+		}
+		return added;
 	}
 }
---
+++ Team_05/src/WorkingAreaTab.java
@@ -0,0 +1,11 @@
+import javax.swing.JPanel;
+
+public class WorkingAreaTab extends JPanel{
+	private String name;
+	
+	WorkingAreaTab()
+	{
+		this.setLayout(null);
+	}
+
+}
