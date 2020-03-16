6af152b9a006a4593d4d9fb78ad4960464abec91
Yijian Hu <yijianhu@asu.edu>
2020-03-16 08:13:10 -0700

    Update TabInfo.java

--- Team_05/src/TabInfo.java
+++ Team_05/src/TabInfo.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
 
+/**
+ * 
+ * @author Yijian Hu
+ *
+ */
 public class TabInfo {
-
+	private String name;
+	private WorkingAreaTab workingAreaTab;
+	private Hashtable<Integer, ButtonBox> shapes;
+	private List<Connection> connections;
+	
+	TabInfo(WorkingAreaTab workingAreaTab)
+	{
+		this.workingAreaTab = workingAreaTab;
+		this.name = workingAreaTab.getName();
+		shapes = new Hashtable<Integer, ButtonBox>();
+		connections = new ArrayList<Connection>();
+	}
+	
+	public void setshapes( Hashtable<Integer, ButtonBox> inshapes) {
+		this.shapes = inshapes;
+	}
+	
+	public Hashtable<Integer, ButtonBox> getshapes() {
+		return shapes;
+	}
+	
+	public void addConnection(Connection conn) {
+		connections.add(conn);
+	}
+	
+	public List<Connection> getConnectionCollection(){
+		return connections;
+	}
+	
+	public WorkingAreaTab getTab()
+	{
+		return workingAreaTab;
+	}
 }
