ca40c9f5c3252310fe0ccd3901573d900a445577
Yijian Hu <yijianhu@asu.edu>
2020-03-16 15:28:32 -0700

    Update RightTabbedPaneController.java

--- Team_05/src/RightTabbedPaneController.java
+++ Team_05/src/RightTabbedPaneController.java
@@ -1,4 +1,115 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.Hashtable;
 
-public class RightTabbedPaneController {
+public class RightTabbedPaneController implements ActionListener, MouseListener, MouseMotionListener{
+	private RightTabbedPane rightTabbedPane;
+	private Hashtable<String, TabInfo> tabs;
+	
+	RightTabbedPaneController()
+	{
+		if(Model.getRightTabbedPane()==null)
+		{
+			Model.setRightTabbedPane(new RightTabbedPane());
+		}
+		rightTabbedPane = Model.getRightTabbedPane();
+		tabs = Model.getTabs();
+	}
+	
+	public boolean addWorkingAreaTab(WorkingAreaTab tab)
+	{
+		boolean added = false;
+		if(tabs == null)
+		{
+			tabs = new Hashtable<String, TabInfo>();
+		}
+		if(tab.getName() == null)
+		{
+			added = false;
+		}
+		else if(!tabs.containsKey(tab.getName()))
+		{
+			tabs.put(tab.getName(), new TabInfo(tab));
+			added = true;
+			rightTabbedPane.add(tab.getName(),tab);
+		}
+		return added;
+	}
+	
+	public void addWorkingAreaTab()
+	{
+		String init_name = "unnamedTab";
+		String name = init_name;
+		int count = 1;
+		if(tabs == null)
+		{
+			tabs = new Hashtable<String, TabInfo>();
+		}
+		while(tabs.containsKey(name))
+		{
+			name = init_name + Integer.toString(count);
+			count++;
+		}
+		WorkingAreaTab tab = new WorkingAreaTab(name);
+		addWorkingAreaTab(tab);
+	}
+	
+	public String getCurrentTabName()
+	{
+		return ((WorkingAreaTab)rightTabbedPane.getSelectedComponent()).getName();
+	}
+	
+	
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
