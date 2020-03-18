d3c5c4a23d184739dffeab74b0ff7f1b21374eb9
Kartik Mathpal <kartikmathpal@Mokshas-MacBook-Pro-2.local>
2020-03-18 16:59:23 -0700

    adds compile logic

--- Team_08/src/CompileFile.java
+++ Team_08/src/CompileFile.java
@@ -3,9 +3,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.*;
 
-/*
-* author: Kartik Mathpal
-* */
 public class CompileFile extends JMenuItem implements ActionListener {
     public CompileFile(String label) {
         super(label);
@@ -25,7 +22,7 @@ public class CompileFile extends JMenuItem implements ActionListener {
         compileCanvas();
 
     }
-    public  void push(Character item, int count){
+    public  void push(Character item,int count){
         if(charMap.containsKey(item)){
             charMap.put(item,charMap.get(item)+1);
         }else{
@@ -35,61 +32,59 @@ public class CompileFile extends JMenuItem implements ActionListener {
     }
 
     /*
-    *
-    * Sample Stub
-    *
-    * */
+     *
+     * Sample Stub
+     *
+     * */
     public void compileCanvas(){
 
-        if(null==charMap|| charMap.isEmpty())
+        if(null==charMap|| charMap.isEmpty()){
             infoBox("No Shapes On Canvas","Failed Case");
+            return;
+        }
+
 
 
         //This will be further updated, is just a stub.
-        if(trackShapes.size()!=0) { //When there is an unconnected shape left on canvas
-            //check for unprocessed shapes
-            Iterator<Shapes> iterator = trackShapes.keySet().iterator();
-            while(iterator.hasNext()){
-                Shapes shp = iterator.next();
-                if(trackShapes.containsKey(shp)){
-                    if(trackShapes.get(shp)==1){
-                        infoBox("Unconnected shapes !!!", "Failed Case");
-                        break;
-                    }
+        //When there is an unconnected shape left on canvas
+        //check for unprocessed shapes
+        Iterator<Shapes> iterator = trackShapes.keySet().iterator();
+        while (iterator.hasNext()) {
+            Shapes shp = iterator.next();
+            if (trackShapes.containsKey(shp)) {
+                if (trackShapes.get(shp) == 1) {
+                    infoBox("Unconnected shapes !!!", "Failed Case");
+                    return ;
                 }
             }
-            //infoBox("Unconnected shapes !!!", "Failed Case");
-            if ((charMap.containsKey('(') && charMap.containsKey(')') && !(charMap.get('(') == charMap.get(')')))) {
-                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
-            }if(charMap.containsKey('<') && charMap.containsKey('>') && !(charMap.get('<') == charMap.get('>'))){
-                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
-            }
-//            if(charMap.get('(') > charMap.get(')') || charMap.get(')') > charMap.get('(')){
-//                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
-//            }
-//            if(charMap.get('<') > charMap.get('>') || charMap.get('>') > charMap.get('<')){
-//                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
-//            }
-            else{
-                infoBox("Compilation successful", "Done");
-            }
-        } else {
-            if ((charMap.containsKey('(') && charMap.containsKey(')') && !(charMap.get('(') == charMap.get(')')))) {
-                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
-            }if(charMap.containsKey('<') && charMap.containsKey('>') && !(charMap.get('<') == charMap.get('>'))){
-                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
-            }
-//            if(charMap.get('(') > charMap.get(')') || charMap.get(')') > charMap.get('(')){
-//                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
-//            }
-//            if(charMap.get('<') > charMap.get('>') || charMap.get('>') > charMap.get('<')){
-//                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
-//            }
-            else{
-                infoBox("Compilation successful", "Done");
-            }
-
         }
+        //infoBox("Unconnected shapes !!!", "Failed Case");
+        if ((charMap.containsKey('(') && charMap.containsKey(')') && !(charMap.get('(') == charMap.get(')')))) {
+            infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
+            return;
+        }if(charMap.containsKey('<') && charMap.containsKey('>') && !(charMap.get('<') == charMap.get('>'))){
+            infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
+            return;
+        }
+        if ((charMap.containsKey('(') && charMap.containsKey(')') && !(charMap.get('(') == charMap.get(')')))) {
+            infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
+            return;
+        }if(charMap.containsKey('<') && charMap.containsKey('>') && !(charMap.get('<') == charMap.get('>'))){
+            infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
+            return;
+        }
+//            if(charMap.get('(') > charMap.get(')') || charMap.get(')') > charMap.get('(')){
+//                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
+//            }
+//            if(charMap.get('<') > charMap.get('>') || charMap.get('>') > charMap.get('<')){
+//                infoBox("Un-balanced Params, Compilation Failed!!!", "Failed Case");
+//            }
+        else{
+            infoBox("Compilation successful", "Done");
+            return;
+        }
+
+
 
     }
     public void infoBox(String infoMessage, String titleBar)
@@ -108,24 +103,6 @@ public class CompileFile extends JMenuItem implements ActionListener {
 
     }
     public static void removeConnectedShapesFromMap(Shapes s){
-
         trackShapes.put(s,0);
-//        Iterator<Shapes> iterator = trackShapes.keySet().iterator();
-//        while(iterator.hasNext()){
-//            //String certification = iterator.next();
-//            Shapes shp = iterator.next();
-//            if(shp.equals(s)){
-//
-//            }
-//        }
-
-        //Read more: https://www.java67.com/2017/06/how-to-remove-entry-keyvalue-from-HashMap-in-java.html#ixzz6GoVAKt77
-
-//        try{
-//            trackShapes.remove(s);
-//            System.out.println(s + " removed!!");
-//        }catch (Exception e){
-//            System.out.println(e.getStackTrace());
-//        }
     }
 }
