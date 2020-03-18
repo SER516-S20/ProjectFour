2248beb94c20c5f819045b99ab205119888b5593
marcushsu123 <marcushsu123@gmail.com>
2020-03-17 23:41:25 -0700

    update compile

--- Team_05/src/Compile.java
+++ Team_05/src/Compile.java
@@ -13,41 +13,58 @@ public class Compile extends Thread {
 	private Hashtable<Integer, ButtonBox> shapes;;
 	private Model model = new Model();
 	private boolean checkValid = false;
-	private int dotNumber;
+	private int dotNumber,leftParenthesisCnt, rightParenthesisCnt;
 
 	public void run() { 
         try { 
             System.out.println ("Thread " + Thread.currentThread().getId() + " is running"); 
-            if(model.getConnectionCollection() != null && model.getshapes() != null) {
-            	connections = model.getConnectionCollection();
-            	shapes = model.getshapes();
-            	//rule1:
-            	for(ButtonBox shapeValue : shapes.values()) {
-            		//System.out.print(shapeValue.());
-            	}
-            	//rule2: All dots in one shape need to be connected with a line.
-            	Set<Integer> keys = shapes.keySet();
-            	for(Integer shapeKey : keys) {
-            		dotNumber = shapes.get(shapeKey).getBtnDots().length;
-            		for(int j = 0 ; j < this.connections.size(); j++) {
-            			Connection finishedconnection = connections.get(j);
-            			if(finishedconnection.getSourceButton().equals(shapeKey) || finishedconnection.getDestButton().equals(shapeKey)) {
-            				dotNumber--;
-            				if(dotNumber == 0) {
-            					checkValid = true;
-            					break;
-            				}
-            			}else {
-            				checkValid = false;
-            			}
-            		}
-            		if(checkValid == false || dotNumber > 0) {
-            			JOptionPane.showMessageDialog(null,"Compile Error!");
-            			break;
-            		}
-            	}
-            	if(checkValid == true) JOptionPane.showMessageDialog(null,"Compile Successfully!");
+            if(model.getConnectionCollection() != null && model.getshapes() == null) {
+            	return;
             }
+            connections = model.getConnectionCollection();
+            shapes = model.getshapes();
+           	//rule1: Only allow one left Parenthesis and one right Parenthesis
+           	leftParenthesisCnt = 0;
+           	rightParenthesisCnt = 0;
+           	for(ButtonBox shapeValue : shapes.values()) {
+           		String str = shapeValue.getSymbol();
+           		switch(str) {
+           			case "(":
+           				leftParenthesisCnt++;
+           				break;
+           			case ")":
+           				rightParenthesisCnt++;
+           				break;
+           			default:
+           				break;
+           		}
+           	}
+           	if(leftParenthesisCnt > 1 || rightParenthesisCnt > 1) {
+       			JOptionPane.showMessageDialog(null,"Compile Error! More than one left parenthesis or right parenthesis");
+       			return;
+           	}
+            //rule2: All dots in one shape need to be connected with a line.
+            Set<Integer> keys = shapes.keySet();
+            for(Integer shapeKey : keys) {
+            	dotNumber = shapes.get(shapeKey).getBtnDots().length;
+            	for(int j = 0 ; j < this.connections.size(); j++) {
+            		Connection finishedconnection = connections.get(j);
+            		if(finishedconnection.getSourceButton().equals(shapeKey) || finishedconnection.getDestButton().equals(shapeKey)) {
+            			dotNumber--;
+            			if(dotNumber == 0) {
+            				checkValid = true;
+            				break;
+           				}
+           			}else {
+           				checkValid = false;
+           			}
+           		}
+           		if(checkValid == false || dotNumber > 0) {
+           			JOptionPane.showMessageDialog(null,"Compile Error!");
+           			break;
+           		}
+           	}
+           	if(checkValid == true) JOptionPane.showMessageDialog(null,"Compile Successfully!");
             System.out.println ("Thread " + Thread.currentThread().getId() + " is finished"); 
         } 
         catch (Exception e) { 
