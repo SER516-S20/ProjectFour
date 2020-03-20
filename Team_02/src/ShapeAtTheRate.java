b175773290e049d89b2ab9777c03e9cd2f083561
Rohit <r4rohit002@gmail.com>
2020-03-19 22:08:58 -0700

    Added repaint function

--- Team_02/src/ShapeAtTheRate.java
+++ Team_02/src/ShapeAtTheRate.java
@@ -19,7 +19,61 @@ public class ShapeAtTheRate extends Icon{
 		repaint();
 	}
 	
+	@Override
+	public void paintComponent(Graphics objGraphics) {
+
+		try {
+			Graphics2D obj2D = (Graphics2D) objGraphics;
+			Shape objRectangle = new Rectangle(this.centerX, this.centerY, width, height);
+			obj2D.draw(objRectangle);
+			obj2D.drawString(text, getMiddlePointX(), getMiddlePointY());
+			
+			obj2D.fillOval(rightUpperDot.getX(), rightUpperDot.getY(), dotSize, dotSize);
+			obj2D.fillOval(rightLowerDot.getX(), rightLowerDot.getY(), dotSize, dotSize);
+			obj2D.fillOval(leftUpperDot.getX(), leftUpperDot.getY(), dotSize, dotSize);
+			obj2D.fillOval(leftLowerDot.getX(), leftLowerDot.getY(), dotSize, dotSize);
+			
+			
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 	
+	
+	public int getMiddlePointX() {
+		return centerX+(width/2);
+	}
+	
+	public int getMiddlePointY() {
+		return centerY+(height/2)+3;
+	}
+	
+	
+	@Override
+	public void setUpPoints() {
+		
+        int x11 = centerX+width-dotMargin-(dotSize/2);
+		
+		int y11 = centerY+(dotMargin)-(dotSize/2);
+		int y21 = centerY+(height)-dotMargin-(dotSize/2);
+		
+		Dot rightUpDot = new Dot(x11, y11, false, true);
+		setRightUpperDot(rightUpDot);
+		
+		Dot rightLoUDot = new Dot(x11, y21, false, true);
+		setRightLowerDot(rightLoUDot);
+		
+		int x1 = centerX+dotMargin-(dotSize/2);
+		int y1 = centerY+(dotMargin)-(dotSize/2);
+		int y2 = centerY+(height)-dotMargin-(dotSize/2);
+		
+		Dot lefttUpDot = new Dot(x1, y1, true, false);
+		setLeftUpperDot(lefttUpDot);
+		
+		Dot lefttLoUDot = new Dot(x1, y2, true, false);
+		setLeftLowerDot(lefttLoUDot);
+		
+	}
 	
 	// On top of parent class Icon,
 	// Implement repaint using centerX, centerY, text, rightDot values.
