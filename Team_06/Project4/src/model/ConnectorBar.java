310320c9e28b88f785d4e23b0816b3cea5507f89
Sheran-12 <54861754+Sheran-12@users.noreply.github.com>
2020-03-20 00:28:11 -0700

    Update ConnectorBar.java

--- Team_06/Project4/src/model/ConnectorBar.java
+++ Team_06/Project4/src/model/ConnectorBar.java
@@ -2,40 +2,38 @@ package model;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
-
 import controller.DrawLine;
 import model.Shape.type;
 
 /**
  * 
- * @author somesh
- * @since 02-28-2020
+ * @Author: Dhananjay, Sheran
+ * @Since: 02-28-2020
+ * @Description: This class inherits the Connector class and is used to define Bars which are used in the different buttons in the application.
  */
 
 public class ConnectorBar extends Connector{
+    public ArrayList <Connector> toConnector = new ArrayList<Connector>();
+    //x and y represents the coordinates for the bar.
+    ConnectorBar(int x, int y, type t){
+	super(t);
+	setBounds(x, y, 10, 40);
+	setBorder(new EmptyBorder(10, 40, 0, 0));
+	new DrawLine(this);
+    }
 	
-	public ArrayList <Connector> toConnector = new ArrayList<Connector>();
-	
-	ConnectorBar(int x, int y, type t){
-		super(t);
-		setBounds(x, y, 10, 40);
-	    setBorder(new EmptyBorder(10, 40, 0, 0));
-	    
-	    new DrawLine(this);
-	}
-	
-	public ArrayList<Connector> getToConnector() {
-		return toConnector;
-	}
+    public ArrayList<Connector> getToConnector() {
+	return toConnector;
+    }
 
-	public void setToConnector(ArrayList<Connector> toConnector) {
-		this.toConnector = toConnector;
-	}
-
-	@Override
+    public void setToConnector(ArrayList<Connector> toConnector) {
+	this.toConnector = toConnector;
+    }
+	
+    // This method is used to paint the bar.
+    @Override
     protected void paintComponent(Graphics g)  {
         if (getModel().isArmed()) {
             g.setColor(Color.LIGHT_GRAY);
