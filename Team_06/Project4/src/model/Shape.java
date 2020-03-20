7d65f4a59870717b65660591de63d7f400785966
Sheran-12 <54861754+Sheran-12@users.noreply.github.com>
2020-03-20 00:58:01 -0700

    Update Shape.java

--- Team_06/Project4/src/model/Shape.java
+++ Team_06/Project4/src/model/Shape.java
@@ -1,31 +1,27 @@
 package model;
 import java.awt.Dimension;
 import java.util.ArrayList;
-
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
 
 /**
  * 
- * @author Somesh, Dhananjay
- * @since 01-29-2020
+ * @Author: Somesh, Dhananjay
+ * @Since: 01-29-2020
+ * @Description: This is the abstract class which all the buttons use to model.
  */
 public class Shape extends JButton{
 	protected String userInput;
 	public enum type {INPUT, OUTPUT};
 	protected int shapeIndex;
-	
 	protected ArrayList <Connector> connectors = new ArrayList<Connector>();
-	
-	
 
 	public Shape(String label, int x, int y){
-		super(label);
-		this.userInput = "";
+	    super(label);
+	    this.userInput = "";
 	    Dimension size = getPreferredSize();
 	    size.width = size.height = Math.max(size.width,size.height);
 	    setPreferredSize(size);
-	    //setBounds(x, y, size.width, size.height);
 	    setBounds(x, y, (int)(size.width * 2.5), size.height);
 	    setBorder(new EmptyBorder(x, y, 5, 5));
 	    setLayout(null);
