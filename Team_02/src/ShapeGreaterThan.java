bc4ae9302c777a12496b499e03e58f01e1896236
Sarvansh Prasher <sarvanshp@gmail.com>
2020-03-19 22:17:21 -0700

    Added shape greater than structure

--- Team_02/src/ShapeGreaterThan.java
+++ Team_02/src/ShapeGreaterThan.java
@@ -1,3 +1,8 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+
 public class ShapeGreaterThan extends Icon{
 
 	private static final long serialVersionUID = 1L;
@@ -8,11 +13,61 @@ public class ShapeGreaterThan extends Icon{
 	private Dot leftUpperDot, leftLowerDot;
 	
 	public ShapeGreaterThan(int x, int y) {
-		this.setCenterX(x);
-		this.setCenterY(y);
-		this.setRightDot(new Dot(x, y, false, true));
+		this.centerX = x;
+		this.centerY = y;
+		setUpPoints();
+		repaint();
 			
 		// call paint component from here, repaint ?!
+	}
+	
+	@Override
+	public void paintComponent(Graphics objGraphics) {
+
+		try {
+			Graphics2D obj2D = (Graphics2D) objGraphics;
+			Shape objRectangle = new Rectangle(this.centerX, this.centerY, width, height);
+			obj2D.draw(objRectangle);
+			obj2D.drawString(text, getMiddlePointX(), getMiddlePointY());
+			
+			
+			obj2D.fillOval(rightDot.getX(), rightDot.getY(), dotSize, dotSize);
+			obj2D.fillOval(leftUpperDot.getX(), leftUpperDot.getY(), dotSize, dotSize);
+			obj2D.fillOval(leftLowerDot.getX(), leftLowerDot.getY(), dotSize, dotSize);
+			
+			
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	
+	public int getMiddlePointX() {
+		return centerX+(width/2);
+	}
+	
+	public int getMiddlePointY() {
+		return centerY+(height/2)+3;
+	}
+	
+	
+	@Override
+	public void setUpPoints() {
+		int x = centerX+width-dotMargin-(dotSize/2);
+		int y = centerY+(height/2)-(dotSize/2);
+		Dot right = new Dot(x, y, false, true);
+		setRightDot(right);
+		
+		int x1 = centerX+dotMargin-(dotSize/2);
+		int y1 = centerY+(dotMargin)-(dotSize/2);
+		int y2 = centerY+(height)-dotMargin-(dotSize/2);
+		
+		Dot lefttUpDot = new Dot(x1, y1, true, false);
+		setLeftUpperDot(lefttUpDot);
+		
+		Dot lefttLoUDot = new Dot(x1, y2, true, false);
+		setLeftLowerDot(lefttLoUDot);
+		
 	}
 	
 	// On top of parent class Icon,
@@ -42,20 +97,4 @@ public class ShapeGreaterThan extends Icon{
 		this.leftLowerDot = leftLowerDot;
 	}
 
-	
-	public int getCenterX() {
-		return centerX;
-	}
-	
-	public void setCenterX(int centerX) {
-		this.centerX = centerX;
-	}
-
-	public int getCenterY() {
-		return centerY;
-	}
-
-	public void setCenterY(int centerY) {
-		this.centerY = centerY;
-	}
 }
