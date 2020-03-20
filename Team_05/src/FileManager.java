cb614e56bcea9a05c250a1437cb71deca695bea5
Yijian Hu <yijianhu@asu.edu>
2020-03-20 16:14:22 -0700

    Update FileManager.java

--- Team_05/src/FileManager.java
+++ Team_05/src/FileManager.java
@@ -82,7 +82,6 @@ public class FileManager {
 						conn.appendChild(position);
 					}
 				}
-				
 			}
 			TransformerFactory transformerFactory = TransformerFactory.newInstance();
 			Transformer transformer = transformerFactory.newTransformer();
@@ -106,7 +105,6 @@ public class FileManager {
 			tabs.clear();
 			pane.removeAll();
 			NodeList nodeList = doc.getElementsByTagName("tab");
-			
 			for(int i = 0;i < nodeList.getLength();i++) {
 				Node node = nodeList.item(i);
 				if (node.getNodeType() == Node.ELEMENT_NODE) {  
@@ -137,7 +135,6 @@ public class FileManager {
 						System.out.println(si.toString());
 					tab.load(shapes);
 					NodeList connList = tabElement.getElementsByTagName("conn");
-					//model.clearConnection();
 					for(int j = 0;j < connList.getLength();j++) {
 						Node conn = connList.item(j);
 						if (conn.getNodeType() == Node.ELEMENT_NODE) {  
@@ -156,7 +153,6 @@ public class FileManager {
 							tabInfo.addConnection(newConn);
 						}
 					}
-
 				}
 			}
 		}
