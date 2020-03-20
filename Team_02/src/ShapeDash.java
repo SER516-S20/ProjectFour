825c9363609326da24adb8bd64006ccb269e1454
Rohit <r4rohit002@gmail.com>
2020-03-19 22:13:33 -0700

    Added draw logic for the shape

--- Team_02/src/ShapeDash.java
+++ Team_02/src/ShapeDash.java
@@ -22,6 +22,46 @@ public class ShapeDash extends Icon{
 	}
 	
 	
+	@Override
+	public void paintComponent(Graphics objGraphics) {
+
+		try {
+			Graphics2D obj2D = (Graphics2D) objGraphics;
+			Shape objRectangle = new Rectangle(this.centerX, this.centerY, width, height);
+			obj2D.draw(objRectangle);
+			obj2D.drawString(text, getMiddlePointX(), getMiddlePointY());
+			
+			obj2D.fillOval(leftDot.getX(), leftDot.getY(), dotSize, dotSize);
+			obj2D.fillOval(rightDot.getX(), rightDot.getY(), dotSize, dotSize);
+				
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	
+	public int getMiddlePointX() {
+		return centerX+(width/2);
+	}
+	
+	public int getMiddlePointY() {
+		return centerY+(height/2)+3;
+	}
+	
+	
+	@Override
+	public void setUpPoints() {
+		int x = centerX+dotMargin-(dotSize/2);
+		int y = centerY+(height/2)-(dotSize/2);
+		Dot left = new Dot(x, y, true, false);
+		setLeftDot(left);
+		
+		int x1 = centerX+width-dotMargin-(dotSize/2);
+		int y1 = centerY+(height/2)-(dotSize/2);
+		Dot right = new Dot(x1, y1, false, true);
+		setRightDot(right);
+		
+	}
 	
 	// On top of parent class Icon,
 	// Implement repaint using centerX, centerY, text, rightDot values.
