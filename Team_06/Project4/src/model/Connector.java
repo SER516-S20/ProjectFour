b72cf9a118f26e561055d97c9bb8d5bdcd4b5bd6
Sheran-12 <54861754+Sheran-12@users.noreply.github.com>
2020-03-20 00:23:03 -0700

    Update Connector.java

--- Team_06/Project4/src/model/Connector.java
+++ Team_06/Project4/src/model/Connector.java
@@ -1,10 +1,14 @@
 package model;
-
 import javax.swing.JButton;
 import javax.swing.border.EmptyBorder;
-
 import model.Shape.type;
 
+/**
+ * 
+ * @Author: Somesh
+ * @Since: 01-29-2020
+ * @Description: This abstract class is used to define connections(lines).
+ */
 public class Connector extends JButton{
 	public type type;
 	public boolean lineFlag;
