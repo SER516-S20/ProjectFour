c63b2619841b7132322bd67c964d11f584b07d99
Sheran-12 <54861754+Sheran-12@users.noreply.github.com>
2020-03-20 00:21:04 -0700

    Update CloseParanthesis.java

--- Team_06/Project4/src/model/CloseParanthesis.java
+++ Team_06/Project4/src/model/CloseParanthesis.java
@@ -1,11 +1,16 @@
 package model;
 import java.awt.Color;
 import java.awt.Graphics;
-
 import controller.Drag;
 import controller.ShapeMouseListener;
 import model.Shape.type;
 
+/**
+ * 
+ * @Author: Somesh
+ * @Since: 01-29-2020
+ * @Description: This model class defines the ')' button, which inherits the Shape abstract class, which is used in the left and right panels of the application.
+ */
 public class CloseParanthesis extends Shape{
 	
 	public CloseParanthesis(int x, int y, boolean rightPanel){
@@ -19,10 +24,9 @@ public class CloseParanthesis extends Shape{
 			this.add(leftMiddle);
 			System.out.println("");
 			new Drag(this);
-
 		}
 	}
-	
+	// This method is used to paint the dots in the button
 	@Override
 	protected void paintComponent(Graphics g) {
         if (getModel().isArmed()) {
@@ -31,10 +35,10 @@ public class CloseParanthesis extends Shape{
         else {
             g.setColor(getBackground());
         }
-        //g.fillRect(0, 0, getSize().width, getSize().height);
+        // This is the funtion used to paint 
         super.paintComponent(g);
     }
-	
+    // This method is used to paint the button
     protected void paintBorder(Graphics g) 
     {
         g.setColor(getForeground());
